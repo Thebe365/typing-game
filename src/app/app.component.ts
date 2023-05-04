4df1b4d1f1b79060d9982036235b13ccb1e096b6
thebe <Thebe@digilink.africa>
2023-05-04 11:49:59 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  
+  // Generate a new sentence
+  fakeText = lorem.sentence();
+  enteredText = ''
+
+  // Check input field each time the user enters a letter
+  inputCount(value: string){
+    
+    
+    let inputLength = value.length - 1 
+    this.enteredText = value
+
+    console.log(this.enteredText)
+
+    if(value[inputLength] == this.fakeText[inputLength]){
+
+      console.log("Its true", `${value[inputLength]} + ${this.fakeText[inputLength]}`)
+    }else{
+      console.log("This aint it cheif", `${value[inputLength]} + ${this.fakeText[inputLength]}`)
+    }
+  }
+
+  getClass(letter: String, text: String){
+    
+    if(!text){
+      return "pending"
+    }
+    return letter === text ? 'correct' : 'incorrect'
+  }
 }
